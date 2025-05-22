7e919704c74928d8673c47c68b550529c9520050
mazelka <mary.banshee@gmail.com>
2025-05-22 12:56:12 +0300

    add s3 upload job
    Signed-off-by: mazelka <mary.banshee@gmail.com>

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,7 +1,19 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 const exec = require('@actions/exec');
 function run() {
     core.info('Starting deploy-s3 action');
+    const bucket = core.getInput('bucket', { required: true });
+    const region = core.getInput('region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    exec.exec('aws', ['s3', 'sync', distFolder, `s3://${bucket}`, '--region', region])
+        .then(() => {
+            core.info('Deployment to S3 completed successfully');
+        })
+        .catch((error) => {
+            core.setFailed(`Deployment to S3 failed: ${error.message}`);
+        });
+    core.info(`Bucket: ${bucket}`);
 }
 run();
