419d0cb1e97345ee1b9799146036983bd42becb9
mazelka <mary.banshee@gmail.com>
2025-05-22 12:29:12 +0300

    add custom action js
    Signed-off-by: mazelka <mary.banshee@gmail.com>

---
+++ .github/actions/deploy-s3-js/main.js
@@ -0,0 +1,7 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
+function run() {
+    core.info('Starting deploy-s3 action');
+}
+run();
